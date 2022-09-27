bacba33eefa46af3c2467bcc7e7cd839f27f7b52
raphaelsoul <raphaelsoul@163.com>
2022-09-27 17:34:16 +0800

    feat(admin): login & register form

--- apps/admin/src/app/pages/auth/login/login.component.ts
+++ apps/admin/src/app/pages/auth/login/login.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { MaterialUIModule } from '../../../material-ui.module';
 
 @Component({
   standalone: true,
   selector: 'app-login',
+  imports: [MaterialUIModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.less'],
 })
-export class LoginComponent implements OnInit {
-  constructor() {}
+export class LoginComponent {
+  title = 'Login';
 
-  ngOnInit(): void {}
+  public form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  submit() {
+    if (this.form.valid) {
+      this.submitEM.emit(this.form.value);
+    }
+  }
+  @Input() error: string | null | undefined;
+
+  @Output() submitEM = new EventEmitter();
 }
--- apps/admin/src/app/pages/auth/register/register.component.ts
+++ apps/admin/src/app/pages/auth/register/register.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MaterialUIModule } from '../../../material-ui.module';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   standalone: true,
+  imports: [MaterialUIModule],
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.less'],
 })
-export class RegisterComponent implements OnInit {
-  constructor() {}
+export class RegisterComponent {
+  title = 'Register';
 
-  ngOnInit(): void {}
+  public form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  submit() {
+    if (this.form.valid) {
+      this.submitEM.emit(this.form.value);
+    }
+  }
+  @Input() error: string | null | undefined;
+
+  @Output() submitEM = new EventEmitter();
 }
