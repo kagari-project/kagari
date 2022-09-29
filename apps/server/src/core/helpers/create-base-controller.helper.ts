eca28bd28d0d9b232296bacaad089d4d576dcbfc
raphaelsoul <raphaelsoul@163.com>
2022-09-29 23:42:34 +0800

    refactor(server): type safed create-base-controller

--- apps/server/src/core/helpers/create-base-controller.helper.ts
+++ apps/server/src/core/helpers/create-base-controller.helper.ts
@@ -8,51 +8,50 @@ import {
   Patch,
   Put,
   Type,
-  UseGuards,
 } from '@nestjs/common';
-import { InjectRepository, Repository } from '@kagari/database';
-import { RbacGuard } from '../../services/rbac/rbac.guard';
+import { FindOneOptions, InjectRepository, Repository } from '@kagari/database';
+
+export type BaseController<Entity> = Type<{
+  findAll(...args: any[]): Promise<{ list: Type<Entity>[]; total: number }>;
+  findOne(...args: any[]): Promise<Type<Entity>>;
+  createOne(...args: any[]): Promise<Type<Entity>>;
+  updateOne(...args: any[]): Promise<Type<Entity>>;
+  deleteOne(...args: any[]): Promise<Type<Entity>>;
+}>;
 
 export function CreateBaseControllerHelper<Entity>(
   entity: Type<Entity>,
   controllerOptions: ControllerOptions,
-): Type {
+): BaseController<Entity> {
   @Controller(controllerOptions)
   class TargetController {
-    constructor(
-      @InjectRepository(entity) private repo: Repository<typeof entity>,
-    ) {}
+    constructor(@InjectRepository(entity) private repo: Repository<Entity>) {}
 
-    @UseGuards(RbacGuard)
     @Get()
     async findAll() {
       const [list, total] = await this.repo.findAndCount();
       return { list, total };
     }
 
-    @UseGuards(RbacGuard)
     @Get(':id')
     async findOne(@Param(':id') id: string) {
-      return this.repo.findOneById(id);
+      return this.repo.findOneOrFail({ where: { id } } as FindOneOptions);
     }
 
-    @UseGuards(RbacGuard)
     @Put()
     async createOne(@Body() data: any) {
       return this.repo.create(data);
     }
 
-    @UseGuards(RbacGuard)
     @Patch(':id')
     async updateOne(@Param(':id') id: string, @Body() data: any) {
       return this.repo.update(id, data);
     }
 
-    @UseGuards(RbacGuard)
     @Delete(':id')
     async deleteOne(@Param(':id') id: string) {
       return this.repo.softDelete(id);
     }
   }
-  return TargetController;
+  return TargetController as unknown as BaseController<Entity>;
 }
