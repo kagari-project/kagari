10d84f9054e29a5e40dffcc585d848ef00be3ccb
raphaelsoul <raphaelsoul@163.com>
2022-10-01 02:06:15 +0800

    feat(admin): design basic resource page layout

--- apps/admin/src/app/pages/user/user.component.ts
+++ apps/admin/src/app/pages/user/user.component.ts
@@ -1,13 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MaterialUIModule } from '../../material-ui.module';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 @Component({
   standalone: true,
+  imports: [MaterialUIModule],
   selector: 'app-user',
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'zh-cn' }],
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
 })
-export class UserComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class UserComponent {
+  title = 'Users';
+  columns = ['id', 'name', 'createdAt', 'updatedAt', 'actions'];
+  dataSource = [
+    { id: 1, name: 'foobar', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 2, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 3, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 4, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 5, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 6, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 7, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 8, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 9, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+    { id: 10, name: 'lorem', createdAt: '2022-01-01', updatedAt: '2022-01-01' },
+  ];
 }
