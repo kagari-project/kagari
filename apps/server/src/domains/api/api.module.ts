c57c7244826a2bd92b2e70c5570430710b6a91ec
raphaelsoul <raphaelsoul@163.com>
2022-10-08 15:17:47 +0800

    refactor(server): add prefix for api domain

--- apps/server/src/domains/api/api.module.ts
+++ apps/server/src/domains/api/api.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from '../../resources/user/user.module';
 import { DemoController } from './demo.controller';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    RouterModule.register([{ path: 'api', module: UserModule }]),
+    UserModule,
+  ],
   controllers: [DemoController],
 })
 export class ApiModule {}
