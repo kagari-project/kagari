84f41728ccc65377babc88a88e2d50d88e34a815
raphaelsoul <raphaelsoul@163.com>
2022-10-12 14:35:35 +0800

    fix(server): supports $withDeleted operator

--- apps/server/src/core/helpers/transform-protocol.helper.ts
+++ apps/server/src/core/helpers/transform-protocol.helper.ts
@@ -5,12 +5,13 @@ import { FindManyOptions } from '@kagari/database';
 export function transformProtocolHelper(
   data: ParsedQueryString,
 ): FindManyOptions {
-  const { $page, $pageSize, $select, $where, $sort } = data;
+  const { $page, $pageSize, $select, $where, $sort, $withDeleted } = data;
   return {
     select: $select as string[],
     where: extractOperators($where),
     order: $sort,
     take: $pageSize,
     skip: $page * $pageSize - $pageSize,
+    withDeleted: $withDeleted,
   };
 }
