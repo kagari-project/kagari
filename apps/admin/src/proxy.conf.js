238ea16c9c6ddf7c976333bbe0831ee0fc021bbc
raphaelsoul <raphaelsoul@163.com>
2022-11-14 16:52:11 +0800

    chore: remove ng app and add storybook

--- apps/admin/src/proxy.conf.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-  {
-    context: ['/api', '/auth'],
-    target: 'http://localhost:3000',
-    changeOrigin: true,
-    logLevel: 'debug',
-    secure: false,
-  },
-];
--- apps/admin/src/types/types.d.ts
+++
@@ -1,5 +0,0 @@
-import '@angular/router';
-
-declare module '@angular/router' {
-  type RouteMetadata = { label: string; url?: string | string[] };
-}
