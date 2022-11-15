a3f6e627f8173e86dc101eb515d8de2e671e4a34
raphaelsoul <raphaelsoul@163.com>
2022-11-15 17:13:50 +0800

    chore: add story book integration

---
+++ components/ui/DemoComponent/index.stories.tsx
@@ -0,0 +1,12 @@
+// Button.stories.js|jsx
+
+import React from 'react';
+
+import { Button } from './index';
+
+export default {
+  title: 'Button',
+  component: Button,
+};
+
+export const Primary = () => <button>Button</button>;
