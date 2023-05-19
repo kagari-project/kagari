4cd193e20aa5757f0731024f442e413db5ce0be0
raphaelsoul <raphaelsoul@163.com>
2023-05-19 15:53:03 +0800

    chore(docs): add snackbar docs

---
+++ components/ui/utils/ProSnackbar/ProSnackbar.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Meta, StoryObj } from "@storybook/react";
+import { SnackbarProvider, snackbar } from './ProSnackbar'
+import Button from "@mui/material/Button";
+
+const meta: Meta<typeof SnackbarProvider> = {
+  title: 'Utils/ProSnackbar',
+  component: SnackbarProvider,
+  tags: ['autodocs'],
+}
+
+export default meta
+type Story = StoryObj<typeof SnackbarProvider>
+
+export const Default: Story = {
+  render() {
+    return (
+      <SnackbarProvider>
+        <Button onClick={() => snackbar.info('hello, baby!')}>trigger</Button>
+      </SnackbarProvider>
+    )
+  }
+}
